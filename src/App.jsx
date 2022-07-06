906975171c704301905acda6189fcb2c11a981b2
Hudson Holanda <hudsonholanda55@gmail.com>
2022-07-06 20:52:31 -0300

    feat: import Header component, Post component, global styles and Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 export function App() {
   return (
     <div>
-      <Post
-        author="Hudson Holanda"
-        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-      />
-      <Post
-        author="Henrique Holanda"
-        content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="Hudson Holanda"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          />
+          <Post
+            author="Henrique Holanda"
+            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+          />
+        </main>
+      </div>
     </div>
   );
 }
