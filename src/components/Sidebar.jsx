919b6f3f8db2bf8168e8dbbed300dd551cde6bf7
Hudson Holanda <hudsonholanda55@gmail.com>
2022-07-06 20:52:00 -0300

    feat: create Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1618388810903-840bb0d15ea5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+      <div className={styles.profile}>
+        <img src="https://github.com/lzhudson.png" alt="" />
+        <strong>Hudson Holanda</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
